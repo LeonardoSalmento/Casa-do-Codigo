b47c648db1727d0d3ba2a6a7ff08a96972469f7c
leonardosalmento <salmento_@hotmail.com>
2021-02-26 16:13:55 -0300

    Adicionando injeção de dependência

---
+++ DataService.cs
@@ -0,0 +1,16 @@
+﻿namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+
+        public DataService(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+        }
+        public void InicializaDB()
+        {
+            contexto.Database.EnsureCreated();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,8 @@ namespace CasaDoCodigo
             services.AddDbContext<ApplicationContext>(options =>
             options.UseSqlServer(connectionString)
             );
+
+            services.AddTransient<IDataService, DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +56,7 @@ namespace CasaDoCodigo
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated();
+            serviceProvider.GetService<IDataService>().InicializaDB();
         }
     }
 }
