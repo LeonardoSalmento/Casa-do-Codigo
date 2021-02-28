f72f3dd4d1353c75f401992954c5f985a7d798e8
leonardosalmento <salmento_@hotmail.com>
2021-02-28 09:15:23 -0300

    Criando pedidos e adicionando produtos no pedido

--- Repositories/PedidoRepository.cs
+++ Repositories/PedidoRepository.cs
@@ -10,8 +10,9 @@ namespace CasaDoCodigo.Repositories
 
     public interface IPedidoRepository
     {
-
-    }   
+        Pedido GetPedido();
+        void AddItem(string codigo);
+    }
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
         private readonly IHttpContextAccessor contextAccessor;
@@ -20,12 +21,49 @@ namespace CasaDoCodigo.Repositories
             this.contextAccessor = contextAccessor;
         }
 
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>().Where(p => p.Codigo == codigo).SingleOrDefault();
+
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontro");
+            }
+
+            var pedido = GetPedido();
+
+            var itemPedido = contexto.Set<ItemPedido>().Where(i => i.Produto.Codigo == codigo && i.Pedido.Id == pedido.Id).SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>().Add(itemPedido);
+                contexto.SaveChanges();
+            }
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet.Where(p => p.Id == pedidoId).SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+                SetPedidoId(pedido.Id);
+            }
+
+            return pedido;
+        }
+
         private int? GetPedidoId()
         {
             return contextAccessor.HttpContext.Session.GetInt32("pedidoId");
         }
 
-        private void SetPedido(int pedidoId)
+        private void SetPedidoId(int pedidoId)
         {
             contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
         }
